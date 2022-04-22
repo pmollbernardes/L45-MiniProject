deec9097c4168989b6832d57e7399253338fda98
Pedro Bernardes <pedrobernar10@gmail.com>
2022-04-22 12:24:42 -0300

    Update experimental setup

---
+++ MPNNLayer.py
@@ -0,0 +1,93 @@
+import torch
+from torch.nn import Linear, Sequential, BatchNorm1d, ReLU
+from torch_scatter import scatter
+
+
+class MPNNLayer(torch.nn.Module):
+    def __init__(self, emb_dim=64, edge_dim=4, aggr='add'):
+        """Message Passing Neural Network Layer
+
+        Args:
+            emb_dim: (int) - hidden dimension `d`
+            edge_dim: (int) - edge feature dimension `d_e`
+            aggr: (str) - aggregation function `\oplus` (sum/mean/max)
+        """
+        super().__init__(aggr=aggr)
+
+        self.emb_dim = emb_dim
+        self.edge_dim = edge_dim
+
+        self.mlp_msg = Sequential(
+            Linear(2*emb_dim + edge_dim, emb_dim), BatchNorm1d(emb_dim), ReLU(),
+            Linear(emb_dim, emb_dim), BatchNorm1d(emb_dim), ReLU()
+        )
+
+        self.mlp_upd = Sequential(
+            Linear(2*emb_dim, emb_dim), BatchNorm1d(emb_dim), ReLU(),
+            Linear(emb_dim, emb_dim), BatchNorm1d(emb_dim), ReLU()
+        )
+
+    def forward(self, h, edge_index, edge_attr):
+        """
+        The forward pass updates node features `h` via one round of message passing.
+        Args:
+            h: (n, d) - initial node features
+            edge_index: (e, 2) - pairs of edges (i, j)
+            edge_attr: (e, d_e) - edge features
+
+        Returns:
+            out: (n, d) - updated node features
+        """
+        out = self.propagate(edge_index, h=h, edge_attr=edge_attr)
+        return out
+
+    def message(self, h_i, h_j, edge_attr):
+        """
+        The `message()` function constructs messages from source nodes j 
+        to destination nodes i for each edge (i, j) in `edge_index`
+
+        Args:
+            h_i: (e, d) - destination node features
+            h_j: (e, d) - source node features
+            edge_attr: (e, d_e) - edge features
+
+        Returns:
+            msg: (e, d) - messages `m_ij` passed through MLP `\psi`
+        """
+        if self.edge_dim:
+            msg = torch.cat([h_i, h_j, edge_attr], dim=-1)
+        else:
+            msg = torch.cat([h_i, h_j], dim=-1)
+        return self.mlp_msg(msg)
+
+    def aggregate(self, inputs, index):
+        """
+        The `aggregate` function aggregates the messages from neighboring nodes,
+        according to the chosen aggregation function ('sum' by default).
+
+        Args:
+            inputs: (e, d) - messages `m_ij` from destination to source nodes
+            index: (e, 1) - list of source nodes for each edge/message in `input`
+
+        Returns:
+            aggr_out: (n, d) - aggregated messages `m_i`
+        """
+        return scatter(inputs, index, dim=self.node_dim, reduce=self.aggr)
+
+    def update(self, aggr_out, h):
+        """
+        The `update()` function computes the final node features by combining the 
+        aggregated messages with the initial node features.
+
+        Args:
+            aggr_out: (n, d) - aggregated messages `m_i`
+            h: (n, d) - initial node features
+
+        Returns:
+            upd_out: (n, d) - updated node features passed through MLP `\phi`
+        """
+        upd_out = torch.cat([h, aggr_out], dim=-1)
+        return self.mlp_upd(upd_out)
+
+    def __repr__(self) -> str:
+        return (f'{self.__class__.__name__}(emb_dim={self.emb_dim}, aggr={self.aggr})')
---
+++ MPNNModel.py
@@ -0,0 +1,57 @@
+import torch
+from torch_geometric.nn import global_mean_pool
+from MPNNLayer import MPNNLayer
+
+
+class MPNNModel(torch.nn.Module):
+    def __init__(self, num_layers=4, emb_dim=64, in_dim=11, edge_dim=4, out_dim=1):
+        """Message Passing Neural Network model for graph property prediction
+
+        Args:
+            num_layers: (int) - number of message passing layers `L`
+            emb_dim: (int) - hidden dimension `d`
+            in_dim: (int) - initial node feature dimension `d_n`
+            edge_dim: (int) - edge feature dimension `d_e`
+            out_dim: (int) - output dimension (fixed to 1)
+        """
+        super().__init__()
+
+        self.lin_in = torch.nn.Linear(in_dim, emb_dim)
+
+        # Stack of MPNN layers
+        self.convs = torch.nn.ModuleList()
+        for layer in range(num_layers):
+            self.convs.append(MPNNLayer(emb_dim, edge_dim, aggr='add'))
+
+        self.pool = global_mean_pool
+
+        # Linear prediction head
+        # dim: d -> out_dim
+        self.lin_pred = torch.nn.Linear(emb_dim, out_dim)
+
+    def forward(self, data):
+        """
+        Args:
+            data: (PyG.Data) - batch of PyG graphs
+
+        Returns: 
+            out: (batch_size, out_dim) - prediction for each graph
+        """
+        h = self.lin_in(data.x)
+
+        for conv in self.convs:
+            h = h + conv(h, data.edge_index, data.edge_attr)
+
+        h_graph = self.pool(h, data.batch)  # (n, d) -> (batch_size, d)
+
+        out = self.lin_pred(h_graph)  # (batch_size, d) -> (batch_size, 1)
+
+        return out
+
+    def reset_parameters(self):
+        for module in self.children():
+            if hasattr(module, 'reset_parameters'):
+                module.reset_parameters()
+            elif isinstance(module, torch.nn.ModuleList):
+                for layer in module.children():
+                    layer.reset_parameters()
